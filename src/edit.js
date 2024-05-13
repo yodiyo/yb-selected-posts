8cf2bc7a856f32200687e1de93aca27ec257ba46
Yorick Brown <yorick.brown@angrycreative.uk>
2024-05-13 01:39:57 +0100

    initial commit

---
+++ src/edit.js
@@ -0,0 +1,83 @@
+import { __ } from '@wordpress/i18n';
+import { useState, useEffect } from '@wordpress/element';
+import { useBlockProps, InspectorControls, RichText } from '@wordpress/block-editor';
+import { PanelBody, ComboboxControl } from '@wordpress/components';
+import { useEntityRecords } from '@wordpress/core-data';
+
+export default function Edit( { attributes, setAttributes } ) {
+	const [ selectedPost, setSelectedPost ] = useState( null );
+	const [ searchText, setSearchText ] = useState( '' );
+
+	// Fetch posts data using useEntityRecords hook
+	const { records: searchResults = [], hasResolved, isResolving } = useEntityRecords(
+		'postType',
+		'post',
+		{
+			search: searchText,
+			per_page: -1,
+			status: 'publish',
+		}
+	);
+
+	const handleComboboxChange = ( newValue ) => {
+		// Find the selected post object from searchResults based on the selected post ID
+		const selectedPostObject = searchResults.find(post => post.id === newValue);
+
+		const { id, title: { rendered }, link } = selectedPostObject;
+		setSelectedPost( { id, title: { rendered }, link } );
+		setAttributes( { selectedPost: { id, title: { rendered }, link } } );
+	};
+
+	useEffect( () => {
+		if( ! hasResolved ) {
+			return;
+		}
+
+		// Set the default selected post as the first item from searchResults
+		if ( selectedPost === null && attributes.selectedPost ) {
+			const savedSelectedPost = searchResults.find( post => post.id === attributes.selectedPost.id );
+			setSelectedPost(savedSelectedPost);
+		}
+	}, [ selectedPost, attributes.selectedPost, hasResolved, searchResults ] );
+
+	return (
+		<div { ...useBlockProps() }>
+			<InspectorControls>
+				<PanelBody title={__('Block Settings')}>
+					<ComboboxControl
+						label = { __( 'Search Posts' ) }
+						options = { searchResults?.map( ( post ) => ( {
+							value: post.id,
+							label: post.title.rendered,
+						} ) ) }
+						onChange = { handleComboboxChange }
+						onInputChange = { setSearchText }
+						selected = { selectedPost ? selectedPost.id : '' }
+						help = 'Select or search for post.'
+					/>
+				</PanelBody>
+			</InspectorControls>
+			<div { ...useBlockProps() }>
+				{ selectedPost ?
+					selectedPost.title && (
+						<RichText.Content
+						 tagName="p"
+						 className="dmg-read-more"
+						 value={ __('Read More: ') + selectedPost.title.rendered }
+						 href={ selectedPost.link }
+					 	/>
+            		)
+					:
+					<p>{ __( 'Select or search for a published post in side panel.' ) }</p>
+				}
+			</div>
+
+		</div>
+	);
+}
+
+Edit.defaultProps = {
+    attributes: {
+        selectedPost: null, // Default value for selectedPost attribute
+    },
+};
