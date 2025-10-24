06a572c5847f11a28013531851e492436c94df01
Yorick <info@theyoricktouch.com>
2025-10-24 02:32:03 +0100

    lint fixes

--- src/edit.js
+++ src/edit.js
@@ -1,6 +1,10 @@
 import { __ } from '@wordpress/i18n';
 import { useState, useEffect } from '@wordpress/element';
-import { useBlockProps, InspectorControls, RichText } from '@wordpress/block-editor';
+import {
+	useBlockProps,
+	InspectorControls,
+	RichText,
+} from '@wordpress/block-editor';
 import { PanelBody, ComboboxControl } from '@wordpress/components';
 import { useEntityRecords } from '@wordpress/core-data';
 
@@ -9,15 +13,15 @@ export default function Edit({ attributes, setAttributes }) {
 	const [searchText, setSearchText] = useState('');
 
 	// Fetch posts data using useEntityRecords hook
-	const { records: searchResults = [], hasResolved, isResolving } = useEntityRecords(
-		'postType',
-		'post',
-		{
-			search: searchText,
-			per_page: -1,
-			status: 'publish',
-		}
-	);
+	const {
+		records: searchResults = [],
+		hasResolved,
+		isResolving,
+	} = useEntityRecords('postType', 'post', {
+		search: searchText,
+		per_page: -1,
+		status: 'publish',
+	});
 
 	useEffect(() => {
 		if (!hasResolved || isResolving) {
@@ -26,21 +30,34 @@ export default function Edit({ attributes, setAttributes }) {
 
 		// Set the default selected post as the first item from searchResults
 		if (selectedPost === null && attributes.selectedPost) {
-			const savedSelectedPost = searchResults.find(post => post.id === attributes.selectedPost.id);
+			const savedSelectedPost = searchResults.find(
+				(post) => post.id === attributes.selectedPost.id
+			);
 			setSelectedPost(savedSelectedPost);
 		}
-	}, [selectedPost, attributes.selectedPost, hasResolved, searchResults]);
+	}, [
+		selectedPost,
+		attributes.selectedPost,
+		hasResolved,
+		isResolving,
+		searchResults,
+	]);
 
 	const handleComboboxChange = (newValue) => {
-
 		if (isNaN(newValue) || newValue === null || !searchResults) {
 			return;
 		}
 
 		// Find the selected post object from searchResults based on the selected post ID
-		const selectedPostObject = searchResults.find(post => post.id === newValue);
+		const selectedPostObject = searchResults.find(
+			(post) => post.id === newValue
+		);
 
-		const { id, title: { rendered }, link } = selectedPostObject;
+		const {
+			id,
+			title: { rendered },
+			link,
+		} = selectedPostObject;
 		setSelectedPost({ id, title: { rendered }, link });
 		setAttributes({ selectedPost: { id, title: { rendered }, link } });
 	};
@@ -51,36 +68,42 @@ export default function Edit({ attributes, setAttributes }) {
 				<PanelBody title={__('Block Settings')}>
 					<ComboboxControl
 						label={__('Search Posts')}
-						options={searchResults ?
-							searchResults.map((post) => ({
-								value: post.id,
-								label: post.title.rendered,
-							}))
-							:
-							[]
+						options={
+							searchResults
+								? searchResults.map((post) => ({
+									value: post.id,
+									label: post.title.rendered,
+								}))
+								: []
 						}
 						onChange={handleComboboxChange}
 						onInputChange={setSearchText}
 						selected={selectedPost ? selectedPost.id : ''}
-						help='Select or search for post.'
+						help="Select or search for post."
 					/>
 				</PanelBody>
 			</InspectorControls>
 			<div {...useBlockProps()}>
-				{selectedPost ?
+				{selectedPost ? (
 					selectedPost.title && (
 						<RichText.Content
 							tagName="p"
 							className="dmg-read-more"
-							value={__('Read More: ') + selectedPost.title.rendered}
+							value={
+								__('Read More: ') +
+								selectedPost.title.rendered
+							}
 							href={selectedPost.link}
 						/>
 					)
-					:
-					<p>{__('Select or search for a published post in side panel.')}</p>
-				}
+				) : (
+					<p>
+						{__(
+							'Select or search for a published post in side panel.'
+						)}
+					</p>
+				)}
 			</div>
-
 		</div>
 	);
 }
--- src/save.js
+++ src/save.js
@@ -12,26 +12,27 @@ import { useBlockProps } from '@wordpress/block-editor';
  * be combined into the final markup, which is then serialized by the block
  * editor into `post_content`.
  *
+ * @param {Object} root0            - The props object.
+ * @param {Object} root0.attributes - The block attributes.
  * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#save
  *
- * @return {Element} Element to render.
+ * @return {Element|null} Element to render.
  */
-export default function save( { attributes } ) {
+export default function save({ attributes }) {
 	const { selectedPost } = attributes;
 
-	if ( ! selectedPost ) {
-		return;
+	if (!selectedPost) {
+		return null;
 	}
 
 	// Extract necessary data from selectedPost
-	const { id, title, link } = selectedPost;
+	const { title, link } = selectedPost;
 
 	// Return the post link markup
 	return (
-		<p className='dmg-read-more' { ...useBlockProps.save() }>
-			<span>{__( 'Read more: ' ) }</span>
-			<a href={ link }>{ title.rendered }</a>
+		<p className="dmg-read-more" {...useBlockProps.save()}>
+			<span>{__('Read more: ')}</span>
+			<a href={link}>{title.rendered}</a>
 		</p>
 	);
 }
-
